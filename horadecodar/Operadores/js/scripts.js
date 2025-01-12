254a79939a03c9ff09f636a183e256e1f6e13806
Cleiton Ferreira da Silva <129744824+CleitoSilva@users.noreply.github.com>
2025-01-12 00:40:53 +0000

    Add files via upload

---
+++ horadecodar/Operadores/js/scripts.js
@@ -0,0 +1,13 @@
+// 1- Number
+console.log(typeof 2);
+console.log(5.14);
+console.log(-127);
+
+// 2 Ops. aritmeticos
+console.log(2 + 3);
+console.log(10 - 5);
+console.log(5 * 2);
+console.log(10 / 2);
+
+console.log(5 + 4 * 2)
+
