aa1b6d9b418c149fd3e5903d862f52555eab2bd4
Cleiton Ferreira da Silva <129744824+CleitoSilva@users.noreply.github.com>
2025-01-12 22:05:19 +0000

    Add files via upload

--- horadecodar/Operadores/js/scripts.js
+++ horadecodar/Operadores/js/scripts.js
@@ -11,3 +11,77 @@ console.log(10 / 2);
 
 console.log(5 + 4 * 2)
 
+//3 Special numbers
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12*"asd");
+
+console.log(typeof NaN);
+
+// 4 - sTRING
+console.log("hola");
+console.log('hola');
+console.log(typeof`hola`);
+
+// 5 simbolos especiais em Strings
+console.log("hola \n mundo");
+console.log("hola \\ mundo");
+console.log("hola \t mundo");
+
+// 6 - concatenacao
+console.log("hola " + "mundo");
+console.log("hola " + 123);
+console.log("hola " + true);
+
+// 7 Interpocao
+
+console.log(`A soma de 2 + 3 e ${2 + 3}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+// 8 - Boolean
+
+console.log( 5 > 20);
+console.log( 5 < 20);
+console.log( 5 == 20);
+console.log(typeof false);
+
+// 9 - Comparacoes
+
+console.log(5 <= 5);
+console.log(10 == 10);
+
+// 10 - identico
+
+console.log(9 == "9");
+console.log(9 === "9");
+console.log(9 !== "9");
+
+// 11 operadores logicos
+
+console.log(true && true);
+console.log(true && false);
+console.log(5 > 2 && 10 > 5);
+console.log(5 > 2 || 10 < 5);
+
+// 12 -  empty values
+console.log(typeof null, typeof undefined);
+console.log(0 == false);
+
+console.log(0 == null);
+console.log(null == undefined);
+
+// 13 - mudancas de tipos
+console.log(Number("123"));
+console.log(String(123));
+
+console.log("10" -1);
+
+
+
+
+
+
+
+
