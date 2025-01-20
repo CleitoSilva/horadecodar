74d7e6f1664304fbb18ff15c80747fc818f661b6
Cleiton Ferreira <cleitonferreira43@hotmail.com>
2025-01-20 14:30:57 +0000

    Upadete

--- horadecodar/Estruturas/js/scripts.js
+++ horadecodar/Estruturas/js/scripts.js
@@ -1,2 +1,43 @@
 // 1 - variaveis
 let nome = "Cleiton";
+
+console.log(nome);
+
+nome = "Cleiton Ferreira";
+
+console.log(nome);
+
+// aqui e um erro de programacao nao posso fazer isso  const e uma variavel que nao pode ser alterada
+
+const idade = 31;
+
+console.log(idade);
+
+idade = 32;
+
+// 2 mais sobre variaveis
+
+// let 2teste = "invalido";   codigo invalido  nao pode comecar com numero
+// let @teste = "invalido"; // codigo invalido nao pode comecar com simbolo
+
+let a=10, b=20, c=30;
+
+console.log(a,b,c);
+
+const nomecompleto = "Cleiton ferreira";
+
+const nomeCompleto = "Cleiton Ferreira";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+// permitido usar _ e $ no inicio de uma variavel
+
+let _teste = "valido";
+
+let $teste = "valido";
+
+console.log(_teste, $teste);
+
+
